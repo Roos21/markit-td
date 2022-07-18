d00c4b0e0dd12df33f96d6d0a2662bd5de6842bb
Roos21 <107746377+Roos21@users.noreply.github.com>
2022-07-18 19:29:04 +0100

    Update settings.py

--- Markit/settings.py
+++ Markit/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wv^-hvga9ink!b4dc$7%exf@z&n_p!#8ibgfgnhixggkzo0zzv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://markit-ch.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
