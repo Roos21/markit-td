ed4525fcd3f5ebe9ea79c8651ffa94e2293dafa6
Roos21 <107746377+Roos21@users.noreply.github.com>
2022-07-19 11:35:08 +0100

    Update settings.py

--- Markit/settings.py
+++ Markit/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wv^-hvga9ink!b4dc$7%exf@z&n_p!#8ibgfgnhixggkzo0zzv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://markit-td.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['https://markit-webapp.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'fr-fr'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
